0e522e817cfba5adfef5872dd66d23b0c1ccfb76
FabriceMilet <milet.fab@gmail.com>
2023-04-13 16:18:45 +0200

    mise en page

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -10,7 +10,7 @@ export default function Home() {
         <title>Trouve la compo</title>
         <meta name="description" content="Jeux, quiz sur le football. Le but est de trouver la compo d'un match de foot" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/tlc.png" />
       </Head>
 
     <main className={styles.container}>
--- src/pages/search.js
+++ src/pages/search.js
@@ -9,7 +9,7 @@ export default function Search({}) {
         <title>Choisis ton match</title>
         <meta name="description" content="Page de recherche du jeux, quiz sur le football. Le but est de trouver la compo d'un match de foot" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/tlc.png" />
       </Head>
     <main className={styles.container}>
    ici viendra la page de recherche
