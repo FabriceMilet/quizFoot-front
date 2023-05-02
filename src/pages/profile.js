df5f8c509271ad14529d7d852ec49d9cafbee85a
FabriceMilet <milet.fab@gmail.com>
2023-05-02 15:59:07 +0200

    suite mise en place page profil

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,31 +2,33 @@ import Head from 'next/head'
 import styles from '../styles/Profile.module.scss'
 import axios from 'axios';
 import Link from 'next/link';
+import { useState } from 'react';
+import Cookies from 'js-cookie';
 
 
 export async function getServerSideProps(context) {
   try {
     const userId = context.req.cookies.id
     const jwt = context.req.cookies.jwt
-       // Vérifie si le token JWT est présent dans les cookies
-       if (!jwt) {
-        return {
-          redirect: {
-            destination: '/login',
-            permanent: false,
-          },
-        }
+    // Vérifie si le token JWT est présent dans les cookies
+    if (!jwt) {
+      return {
+        redirect: {
+          destination: '/login',
+          permanent: false,
+        },
       }
-   
+    }
+
     const res = await axios(`http://localhost:1337/api/users/${userId}`);
     const data = res.data;
 
-  return {
-    props: { user: data },
-  }
-
-   
+    return {
+      props: { user: data },
     }
+
+
+  }
   catch (err) {
     console.error(err)
     return {
@@ -35,11 +37,35 @@ export async function getServerSideProps(context) {
   }
 }
 
-const Profile = ({user}) => {
-console.log('user', user);
-// console.log('ddd', document.cookie);
+const Profile = ({ user }) => {
 
-// on va pouvoir mettre le nom de l'user comme titre
+  const [newUsername, setNewUsername] = useState("");
+ //  console.log('user', user);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const id = Cookies.get('id')
+    const jwt = Cookies.get('jwt')
+    try {
+      await axios.put(
+        `${process.env.NEXT_PUBLIC_STRAPI_URL}/users/${id}`,
+        {
+          username: newUsername,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${jwt}`
+          },
+        }
+      );
+      window.location.reload()
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -48,21 +74,36 @@ console.log('user', user);
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/images/tlc.png" />
       </Head>
-    <main className={styles.container}>
-      <h1>Profil de {user.username}</h1>
-      <div>
-        <p>Nombre de bonnes réponses au cumul de tous tes quiz : {user.result}</p>
-        <p>Va voir ton classement par rapport aux autres joueurs <Link href="/ranking"><button>Go</button></Link></p>
-      </div>
-      <div>
-        <p>Email : {user.email}</p>
-        <p>Tu veux modifier ton mot de passe ?</p>
-        
+      <main className={styles.container}>
+        <h1>Profil de {user.username}</h1>
+       
+        <div className={styles.containerInfo}>
+          <p>Email : {user.email}</p>
+          <p>Tu veux modifier ton pseudo ?</p>
+          <form className={styles.containerForm} onSubmit={handleSubmit}>
+            
+            
+            <label htmlFor="newUsername">Nouveau pseudo</label>
+            <input
+              type="text"
+              id="newUsername"
+              name="newUsername"
+              value={newUsername}
+              onChange={(e) => setNewUsername(e.target.value)}
+            />
+         
+            <button type="submit">Modifier le pseudo </button>
+          </form>
         </div>
 
-   
-    </main>
+        <div className={styles.containerRanking}>
+          <p>Nombre de bonnes réponses au cumul de tous tes quiz : {user.result}</p>
+          <p>Va voir ton classement par rapport aux autres joueurs <Link href="/ranking"><button>Go</button></Link></p>
+        </div>
+
+      </main>
     </>
   )
-}
+};
+
 export default Profile;
